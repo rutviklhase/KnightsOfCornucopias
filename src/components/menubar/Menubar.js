170a36452b887140c988c0889e439a7ef0661a41
rutviklhase <rutvik.lhase@genmills.com>
2022-12-05 22:30:04 +0530

    intial commit

---
+++ src/components/menubar/Menubar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./menubar.scss"
+
+export default function Menubar()
+{
+    return (
+        <div className="Menubar">
+            <nav> 
+                <list>
+                    <li> 
+                        Home
+                    </li>
+                    <li>
+                        Team
+                    </li>
+                    
+                </list>
+            </nav>
+        </div>
+    )
+}
